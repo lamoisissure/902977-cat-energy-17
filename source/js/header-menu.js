adb946ef5781751ea9c0944af201e2c553ad6363
Irina Kim <lamoisissure@gmail.com>
2019-08-16 14:18:43 +0300

    Внесены правки по 5 заданию

--- source/js/header-menu.js
+++ source/js/header-menu.js
@@ -5,11 +5,6 @@ var navToggle = document.querySelector(".header-menu__button");
 navMenu.classList.remove("header-menu--nojs");
 
 navToggle.addEventListener("click", function() {
-  if (navMenu.classList.contains("header-menu--closed")) {
-    navMenu.classList.remove("header-menu--closed");
-    navMenu.classList.add("header-menu--opened");
-  } else {
-    navMenu.classList.remove("header-menu--opened");
-    navMenu.classList.add("header-menu--closed");
-  }
+  navMenu.classList.toggle("header-menu--opened");
+  navMenu.classList.toggle("header-menu--closed");
 });
